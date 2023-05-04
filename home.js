758ad4c8c008eea559d3ba9b6d0e67b01a1c404f
Harsha <harsha@sais-MacBook-Air.local>
2023-05-04 12:00:14 +0530

    first commit

--- home.js
+++ home.js
@@ -1,9 +1,26 @@
-var icon = document.getElementById("icon");
+
+        var icon = document.getElementById("icon");
          icon.onclick = function(){
             document.body.classList.toggle("dark-mode");
+            
             if(document.body.classList.contains("dark-mode")){
                 icon.src = "images/light.png";
             }else{
                 icon.src = "images/moon.png";
-            }
-         }
+            } 
+            
+        }
+ 
+        function loadCoupon(){
+            document.getElementById('coupon').style.visibility = 'visible';
+            document.getElementById('main').style.opacity = '0.3'
+        }
+        
+        function closeCoupon(){
+            document.getElementById('coupon').style.visibility = 'hidden';
+            document.getElementById('main').style.opacity = '1'
+        }
+
+        
+       
+  
